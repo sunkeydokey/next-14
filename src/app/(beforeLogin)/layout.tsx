240ae4981422996046d0d149ef6b9bfe9c3feaba
sunkeydokey <sunkeydokey@gmail.com>
2023-12-10 23:04:29 +0900

    Parrell Routes, Intercepting Routes

---
+++ src/app/(beforeLogin)/layout.tsx
@@ -0,0 +1,17 @@
+import type { ReactNode } from 'react';
+
+import styles from '@/app/(beforeLogin)/_component/main.module.css';
+
+type Props = {
+  children: ReactNode;
+  modal: ReactNode;
+};
+
+export default function Layout({ children, modal }: Props) {
+  return (
+    <div className={styles.container}>
+      {children}
+      {modal}
+    </div>
+  );
+}
